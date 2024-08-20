bf3ba1fc2bcd61ef9923a9d8b5ef989f7b9f7fe0
thomasyedhu24 <thomasyedhu@gmail.com>
2024-08-20 15:17:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "15529802")
+    API_HASH = environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '821215952').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
@@ -14,9 +14,9 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL',''))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1002164715986'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "OFF")  # FORCE SUB ON - OFF
 
 
 class temp(object): 
